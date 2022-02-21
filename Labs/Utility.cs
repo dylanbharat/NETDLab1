d7c93f73124edc934a4f68bafd2376d68a9743fa
dylanbharat <100160091+dylanbharat@users.noreply.github.com>
2022-02-21 14:09:15 -0500

    Add files via upload

---
+++ Labs/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1___Average_Units_Shipped___Dylan_Bharat
+{
+    internal class Utility
+    {
+        public static Boolean isInterger(string input )
+        {
+            int number;
+            return Int32.TryParse(input, out number);
+        }
+
+        public static Boolean isInRange(int min, int max, string input)
+        {
+            int number;
+            if (Int32.TryParse(input, out number))
+            {
+                return min <= number && number <= max;
+            }
+            return false;
+        }
+
+        public static Boolean isNumber(KeyEventArgs e)
+        {
+            return false;
+        }
+    }
+}
